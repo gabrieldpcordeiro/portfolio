f66fca2e274920c85bd99d9bb22aa530dd81a782
Gabriel C <gpcordeiro@hotmail.com>
2023-09-17 21:13:07 -0700

    fixed about layout and add smooth scroll

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,24 +1,24 @@
 // About.tsx
 
-import React, { useState, useEffect, useRef } from 'react';
-import { About as AboutType } from '../types/types';
-import { frontEndSkills, backEndSkills, databaseSkills, otherSkills } from '../data/skillsData';
-import { getImageByName } from '../utils/imageUtils';
+import React, {useState, useEffect, useRef} from 'react';
+import {About as AboutType} from '../types/types';
+import {frontEndSkills, backEndSkills, databaseSkills, otherSkills} from '../data/skillsData';
+import {getImageByName} from '../utils/imageUtils';
 import '../styles/About.css'; // Import the styles for the About component
 
 interface AboutProps {
     about: AboutType;
 }
 
-const About: React.FC<AboutProps> = ({ about }) => {
-    const { bio, imageUrl, skills } = about;
+const About: React.FC<AboutProps> = ({about}) => {
+    const {bio, imageUrl, skills} = about;
     const [imageSrc, setImageSrc] = useState<string | null>(null);
     const aboutRef = useRef<HTMLDivElement | null>(null);
     const skillsRef = useRef<HTMLDivElement | null>(null);
 
     useEffect(() => {
         const fetchImage = async () => {
-            const src = await getImageByName({ name: imageUrl, alt: 'My Profile Picture' });
+            const src = await getImageByName({name: imageUrl, alt: 'My Profile Picture'});
             setImageSrc(src);
         };
         fetchImage();
@@ -48,58 +48,64 @@ const About: React.FC<AboutProps> = ({ about }) => {
         };
     }, [imageUrl]);
 
+    // About.tsx
+
     return (
         <div ref={aboutRef} className="about">
-            <h1>About</h1>
-            <div className="picture">
-                {imageSrc ? (
-                    <img src={imageSrc} alt="My Profile Picture" />
-                ) : (
-                    <div>No Image Found</div>
-                )}
-            </div>
-            <div className="bio">
-                <p>{bio}</p>
-            </div>
-            <div ref={skillsRef} className="skills">
-                <h2>Skills</h2>
-                <div className="skills-categories">
-                    <div className="category">
-                        <h3>Front-End</h3>
-                        <ul className="skill-list">
-                            {frontEndSkills.map((skill, index: number) => (
-                                <li key={index}>{skill}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="category">
-                        <h3>Back-End</h3>
-                        <ul className="skill-list">
-                            {backEndSkills.map((skill, index: number) => (
-                                <li key={index}>{skill}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="category">
-                        <h3>Database</h3>
-                        <ul className="skill-list">
-                            {databaseSkills.map((skill, index: number) => (
-                                <li key={index}>{skill}</li>
-                            ))}
-                        </ul>
-                    </div>
-                    <div className="category">
-                        <h3>Other</h3>
-                        <ul className="skill-list">
-                            {otherSkills.map((skill, index: number) => (
-                                <li key={index}>{skill}</li>
-                            ))}
-                        </ul>
+            <h1 className="title">About</h1>
+            <div className="content">
+                <div className="picture">
+                    {imageSrc ? (
+                        <img src={imageSrc} alt="My Profile Picture"/>
+                    ) : (
+                        <div>No Image Found</div>
+                    )}
+                </div>
+                <div className="bio">
+                    <h2>Bio</h2>
+                    <p>{bio}</p>
+                    <div ref={skillsRef} className="skills">
+                        <h2>Skills</h2>
+                        <div className="skills-categories">
+                            <div className="category">
+                                <h3>Front-End</h3>
+                                <ul className="skill-list">
+                                    {frontEndSkills.map((skill, index: number) => (
+                                        <li key={index}>{skill}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="category">
+                                <h3>Back-End</h3>
+                                <ul className="skill-list">
+                                    {backEndSkills.map((skill, index: number) => (
+                                        <li key={index}>{skill}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="category">
+                                <h3>Database</h3>
+                                <ul className="skill-list">
+                                    {databaseSkills.map((skill, index: number) => (
+                                        <li key={index}>{skill}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                            <div className="category">
+                                <h3>Other</h3>
+                                <ul className="skill-list">
+                                    {otherSkills.map((skill, index: number) => (
+                                        <li key={index}>{skill}</li>
+                                    ))}
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     );
+
 };
 
 export default About;
--- src/data/aboutData.ts
+++ src/data/aboutData.ts
@@ -2,7 +2,7 @@ import {About} from "../types/types";
 export const AboutData: About[] = [
 
     {
-        bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        bio:'Experienced software developer with a track record of delivering high-quality solutions, leveraging a wide range of technologies in agile environments. Committed to ongoing learning and excelling in dynamic team settings',
         imageUrl: 'profile-image.jpeg',
         skills: ['React', 'TypeScript'],
     }
